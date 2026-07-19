f7246319370d79791a1f3241047a097c1fa359b1
EHLO1 <joseph.ley@outlook.com>
2026-07-18 22:03:03 -0400

    Changed first server name

---
+++ docker/komodo/install-periphery.sh
@@ -0,0 +1,5 @@
+curl -sSL https://raw.githubusercontent.com/moghtech/komodo/main/scripts/setup-periphery.py \
+  | sudo python3 - \
+  --core-address="ws://10.0.10.3:9120" \
+  --connect-as="Manager" \
+  --onboarding-key=""
