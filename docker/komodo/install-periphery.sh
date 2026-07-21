6e08e606618c036d7d4ee80d049f5739d6ebdb7f
EHLO <joseph.ley@outlook.com>
2026-07-21 08:09:57 -0500

    Continued improvements on formatting and syntax

--- docker/komodo/install-periphery.sh
+++ docker/komodo/install-periphery.sh
@@ -1,5 +1,5 @@
 curl -sSL https://raw.githubusercontent.com/moghtech/komodo/main/scripts/setup-periphery.py \
   | sudo python3 - \
   --core-address="ws://10.0.10.3:9120" \
-  --connect-as="Manager" \
+  --connect-as="$HOSTNAME" \
   --onboarding-key=""
